4fc459dc1bfacbd1b955880dd347624428ea43ed
soohyun99 <yeo7764@gmail.com>
2021-04-10 15:12:00 +0900

    login change3

--- server/urls.py
+++ server/urls.py
@@ -3,11 +3,16 @@ from server.views import *
 from django.conf.urls.static import static
 from django.conf import settings
 from django.conf.urls import url
+from django.urls import path, include
 
 router = routers.DefaultRouter()
-router.register(r'login', LoginViewSet)
+#router.register(r'login', LoginViewSet)
 router.register(r'store', StoreViewSet)
 router.register(r'bigmarket', BigmarketViewSet)
 
 urlpatterns = router.urls
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns = [
+    path('/create', CreateView.as_view()),
+    path('/login', LoginView.as_view())
+]
--- server/views.py
+++ server/views.py
@@ -10,13 +10,58 @@ from rest_framework.parsers import MultiPartParser
 from django_filters.rest_framework import DjangoFilterBackend
 from server.filter import *
 from server.models import *
+from rest_framework import views
+import json
 
-class LoginViewSet(viewsets.ModelViewSet):
-    queryset = Login.objects.all()
-    serializer_class = LoginSerializer
-    parser_classes = [MultiPartParser]
-    filter_backends = [DjangoFilterBackend]
-    filterset_class = ProduceFilter
+from django.views import View
+from django.http import JsonResponse
+
+#class LoginViewSet(viewsets.ModelViewSet):
+#    queryset = Login.objects.all()
+#    serializer_class = LoginSerializer
+#    if Login.objects.filter(id=id).exists():
+#        account = Login.objects.get(id=id)
+#        if account.pw == pw:
+#            return JsonResponse({'message': 'Login SUCCESS'}, status=200)
+#        return HttpResponse(status=401)
+
+class CreateView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        Login(
+            id     = data['id'],
+            pw    = data['pw'],
+            email = data['email'],
+            nickname = data['nickname']
+        )
+        if Login.objects.filter(id = data['id']).exists() == True:
+            return JsonResponse({"message" : "이미 존재하는 아이디입니다."}, status = 401)
+        else:
+            Login.objects.create(id = data['id'], nickname = data['nickname'], pw = data['pw'], email = data['email'])
+            return JsonResponse({"message" : "회원으로 가입되셨습니다."}, status = 200)
+
+    def get(self, request):
+        login = Login.objects.values()
+        return JsonResponse({"data": list(login)}, status=200)
+
+class LoginView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        Login(
+            id=data['id'],
+            pw=data['pw'],
+            email=data['email'],
+            nickname=data['nickname']
+        )
+
+        if Login.objects.filter(id = data['id'], pw = data['pw']).exists() == True :
+            return JsonResponse({"message": "로그인에 성공하셨습니다."}, status = 200)
+        else:
+            return JsonResponse({"message" : "아이디나 비밀번호가 일치하지 않습니다."}, status = 401)
+
+    def get(self, request):
+        login = Login.objects.values()
+        return JsonResponse({"list" : list(login)}, status = 200)
 
 class StoreViewSet(viewsets.ModelViewSet):
     queryset = Store.objects.all()
