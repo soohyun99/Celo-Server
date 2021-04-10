f1048c867fa57cafbb5ad507cd98dbddd6681c75
soohyun99 <yeo7764@gmail.com>
2021-04-10 21:56:16 +0900

    word to number

--- server/views.py
+++ server/views.py
@@ -38,10 +38,10 @@ class CreateView(View):
             nickname = data['nickname']
         )
         if Login.objects.filter(id = data['id']).exists() == True:
-            return JsonResponse({"message" : "이미 존재하는 아이디입니다."}, status = 401)
+            return JsonResponse({"message" : "0"}, status = 401)
         else:
             Login.objects.create(id = data['id'], nickname = data['nickname'], pw = data['pw'], email = data['email'])
-            return JsonResponse({"message" : "회원으로 가입되셨습니다."}, status = 200)
+            return JsonResponse({"message" : "1"}, status = 200)
 
     def get(self, request):
         login = Login.objects.values()
@@ -55,9 +55,9 @@ class LoginView(View):
             pw=data['pw']
         )
         if Login.objects.filter(id = data['id'], pw = data['pw']).exists() == True :
-            return JsonResponse({"message": "로그인에 성공하셨습니다."}, status = 200)
+            return JsonResponse({"message": "1"}, status = 200)
         else:
-            return JsonResponse({"message" : "아이디나 비밀번호가 일치하지 않습니다."}, status = 401)
+            return JsonResponse({"message" : "0"}, status = 401)
 
     def get(self, request):
         login = Login.objects.values()
