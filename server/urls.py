fbcf130af948511edee166536ed5832d4dfb12a0
soohyun99 <yeo7764@gmail.com>
2021-04-10 15:18:46 +0900

    Update urls.py

--- server/urls.py
+++ server/urls.py
@@ -9,8 +9,8 @@ router = routers.DefaultRouter()
 #router.register(r'login', LoginViewSet)
 router.register(r'store', StoreViewSet)
 router.register(r'bigmarket', BigmarketViewSet)
-router.register(r'create', CreateView)
-router.register(r'login', LoginView)
+router.register(r'create', CreateView.as_view())
+router.register(r'login', LoginView.as_view())
 
 urlpatterns = router.urls
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
